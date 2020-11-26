389b8a403138acea120342b3f5f51806454adc7d
Yakunovich <54987759+Yakunovich@users.noreply.github.com>
2020-11-26 19:12:21 +0300

    Update RevolveclothingTest.java

--- src/test/java/RevolveclothingTest.java
+++ src/test/java/RevolveclothingTest.java
@@ -3,7 +3,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Keys;
@@ -23,24 +25,48 @@ public class RevolveclothingTest {
     @Test
     public void addShoesToBagTest() throws InterruptedException {
         driver.get("https://www.revolve.com/women/?navsrc=main");
-        WebElement closeBanner = driver.findElement(By.xpath("//*[@id='ntf_dialog_close']"));
+        WebElement closeBanner = new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.presenceOfElementLocated(By
+                        .xpath("//*[@id='ntf_dialog_close']")));
+
         closeBanner.click();
-        WebElement inputString = driver.findElement(By.xpath("//*[@id='search_term_new']"));
+
+        WebElement inputString = new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.presenceOfElementLocated(By.
+                        xpath("//*[@id='search_term_new']")));
+
         inputString.sendKeys("Vans");
+
         inputString.sendKeys(Keys.ENTER);
-        WebElement pickShoesButton = driver.findElement(By.xpath("//*[@id='VANX-UZ2']/div/div/a[1]"));
+
+        WebElement pickShoesButton = new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.presenceOfElementLocated(By.
+                        xpath("//*[@id='VANX-UZ2']/div/div/a[1]")));
         pickShoesButton.click();
-        WebElement pichSizeButton = driver.findElement(By.xpath("//*[@id='size-ul']/li[1]/label"));
+
+        WebElement pichSizeButton = new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.presenceOfElementLocated(By.
+                        xpath("//*[@id='size-ul']/li[1]/label")));
         pichSizeButton.click();
-        WebElement addToBagButton = driver.findElement(By.xpath("//*[@id='addtobagbutton']"));
+
+        WebElement addToBagButton = new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.presenceOfElementLocated(By.
+                        xpath("//*[@id='addtobagbutton']")));
+
         addToBagButton.click();
+
         driver.get("https://www.revolve.com/r/ShoppingBag.jsp?navsrc=header");
-        List<WebElement> bagItems = driver.findElements(By.xpath("//*[@id='bag_item_VANX-UZ2_Mens_4_Womens_5_5']"));
+
+        List<WebElement> bagItems = new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.
+                        xpath("//*[@id='bag_item_VANX-UZ2_Mens_4_Womens_5_5']")));
+
         Assert.assertTrue(bagItems.size()>0);
     }
 
     @AfterMethod(alwaysRun = true)
     public void browserTearDown() {
         driver.quit();
+        driver = null;
     }
 }
